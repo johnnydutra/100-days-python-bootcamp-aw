013888c6e28e7a403719b7b80faea866de8eadb1
Johnny Dutra <johnny.dutra@gmail.com>
2023-11-28 12:31:35 -0300

    adds day 2 content

---
+++ 002_tip_calculator/bmi.py
@@ -0,0 +1,6 @@
+heigth = input("Enter your height in m: ")
+weigth = input("Enter your weight in kg: ")
+
+bmi = float(weigth) / float(heigth) ** 2
+
+print(bmi)
---
+++ 002_tip_calculator/life_in_weeks.py
@@ -0,0 +1,5 @@
+age = input("What's your age? ")
+
+weeks_left = 4000 - (int(age) * 52)
+
+print(f"Your have {weeks_left} weeks left")
