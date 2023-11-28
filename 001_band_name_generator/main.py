f8b13ba6539afd3f14be331b87984cfeb72965c0
Johnny Dutra <johnny.dutra@gmail.com>
2023-11-28 11:22:29 -0300

    adds day 1 content

---
+++ 001_band_name_generator/main.py
@@ -0,0 +1,14 @@
+# print("Hello world!")
+# print("Hello world!\nHello world!")
+# print("Hello" + " "  + "Angela")
+
+# input("What is your name?")
+
+# print("Hello " + input("What is your name?"))
+
+
+# print(len(input("What is your name?")))
+
+name = input("What is your name?")
+length = len(name)
+print(name, length)
