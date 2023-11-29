37295d80a06de79fb8390de08416fde621228fc9
Johnny Dutra <johnny.dutra@gmail.com>
2023-11-29 17:54:13 -0300

    adds final day 8 content

--- 008_caesar_cipher/caesar_cipher.py
+++ 008_caesar_cipher/caesar_cipher.py
@@ -1,8 +1,6 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(text, shift, direction):
   returned_text = ""
@@ -10,11 +8,29 @@ def caesar(text, shift, direction):
   if direction == "decode":
     shift *= -1
 
-  for letter in text:
-    position = alphabet.index(letter)
-    new_position = position + shift
-    returned_text += alphabet[new_position]
-    
+  for char in text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift
+      returned_text += alphabet[new_position]
+    else:
+      returned_text += char
+      
   print(f"The {direction}d text is {returned_text}")
 
-caesar(text=text, shift=shift, direction=direction)
+print(logo)
+
+should_continue = True
+while should_continue == True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift % 26
+
+  caesar(text=text, shift=shift, direction=direction)
+
+  prompt = input("Type 'yes' if you want to go again. Otherwise, type 'no'\n")
+  if prompt == "no":
+    should_continue = False
+    print("Goodbye!")
