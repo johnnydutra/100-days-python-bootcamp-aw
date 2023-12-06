13d347ba1a67ab24df04a7a9d8ef4a7bbe3531d3
Johnny Dutra <johnny.dutra@gmail.com>
2023-12-06 15:36:54 -0300

    updates day 17 challenge question source

--- b_intermediate/017_quizbrain/main.py
+++ b_intermediate/017_quizbrain/main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 
-for item in question_data:
-    question = Question(item["text"], item["answer"])
+for item in question_data["results"]:
+    question = Question(item["question"], item["correct_answer"])
     question_bank.append(question)
 
 quizbrain = QuizBrain(question_bank)
